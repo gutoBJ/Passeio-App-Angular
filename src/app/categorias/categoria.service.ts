9c4c9f47e1d08d241b001b913ba94a887b3083a2
Augusto <98566110+gutoBJ@users.noreply.github.com>
2026-06-15 10:02:51 -0300

    feat: deploy da api e build com docker

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'
 
 @Injectable({ // É um decorador que diz ao Angular: "essa classe pode ser injetada em outras classes"
   // O providedIn: 'root' significa que o Angular cria uma única instância desse service para o app inteiro. Então quando você injeta o PacienteService em dois componentes diferentes, os dois estão usando o mesmo objeto.
@@ -10,10 +11,12 @@ import { Observable } from 'rxjs';
 })
 export class CategoriaService {
 
+   apiUrl: string = environment.apiUrl + '/categorias'
+
   constructor(private http: HttpClient) { }
 
   salvar(categoria: Categoria) : Observable<Categoria> {
-    return this.http.post<Categoria>('http://localhost:3000/categorias', categoria)
+    return this.http.post<Categoria>(this.apiUrl, categoria)
   }
 
   // Observable - É um objeto do RxJS que representa um valor que vai chegar no futuro — como uma resposta HTTP.
@@ -23,6 +26,6 @@ export class CategoriaService {
   // .subscribe()Executa o Observable e recebe o resultado
 
   obterTodas() : Observable<Categoria[]> {
-    return this.http.get<Categoria[]>('http://localhost:3000/categorias')
+    return this.http.get<Categoria[]>(this.apiUrl)
   }
 }
